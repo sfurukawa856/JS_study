728e115928fbda1bf33c60ffd009caf9a4d3144e
furukawa <tennis856@gmail.com>
2022-03-26 21:25:24 +0900

    class

--- practice/main.js
+++ practice/main.js
@@ -1,49 +1,48 @@
-function stopWatch(options) {
-	function addMsg(message) {
+class StopWatch {
+	constructor(options = {}) {
+		this.options = options;
+	}
+
+	init() {
+		let { color, backgoundColor } = this.options;
+
+		color = color || 'lightblue';
+		backgoundColor = backgoundColor || 'black';
+
+		const display = document.querySelector('.display');
+		display.style.color = color;
+		display.style.backgroundColor = backgoundColor;
+
+		this.logElm = document.querySelector('.log');
+
+		let timer = null;
+		const startBtn = document.querySelector('.startBtn');
+		startBtn.addEventListener('click', function () {
+			if (timer === null) {
+				let seconds = 0;
+				timer = setInterval(function () {
+					seconds++;
+					display.innerHTML = seconds;
+					console.log(seconds);
+				}, 1000);
+				this.addMsg('開始');
+			}
+		});
+	}
+
+	addMsg(message) {
 		const msgElm = document.createElement('div');
 		const now = new Date();
 		msgElm.innerHTML = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒 ${message}`;
 		const logElm = document.querySelector('.log');
 		logElm.appendChild(msgElm);
 	}
-
-	options = options || {};
-	const color = options.color || 'lightblue';
-	const backgroundColor = options.backgroundColor || 'black';
-
-	const display = document.getElementsByClassName('display')[0];
-	display.style.color = color;
-	display.style.backgroundColor = backgroundColor
-	const startBtn = document.getElementsByClassName('startBtn')[0];
-	let timer = null;
-
-	startBtn.addEventListener('click', function () {
-		if (timer === null) {
-			let seconds = 0;
-			timer = setInterval(function () {
-				seconds++;
-				display.innerHTML = seconds;
-				console.log(seconds);
-			}, 1000);
-			addMsg('開始');
-		}
-	});
-
-	const stopBtn = document.getElementsByClassName('stopBtn')[0];
-	stopBtn.addEventListener('click', function () {
-		if (timer !== null) {
-			console.log('stop:' + timer);
-			clearInterval(timer);
-			timer = null;
-			addMsg('終了');
-		}
-	});
-};
+}
 
 const options = {
 	color: 'lightgreen',
 	backgroundColor: '#333'
 }
 
-stopWatch(options);
-// stopWatch({ 'backgroundColor': 'red' });
+const stopWatch = new StopWatch(options);
+stopWatch.init();
