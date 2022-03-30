4199c369649774c709d227014c465313ad2cb41a
furukawa <tennis856@gmail.com>
2022-03-30 21:13:16 +0900

    photoViewerClass

--- practice/main.js
+++ practice/main.js
@@ -1,48 +1,48 @@
-class StopWatch {
-	constructor(options = {}) {
-		this.options = options;
+class PhotoViewer {
+	constructor(rootElm, images) {
+		this.rootElm = rootElm;
+		this.images = images;
+		this.currentIndex = 0;
 	}
 
 	init() {
-		let { color, backgoundColor } = this.options;
-
-		color = color || 'lightblue';
-		backgoundColor = backgoundColor || 'black';
-
-		const display = document.querySelector('.display');
-		display.style.color = color;
-		display.style.backgroundColor = backgoundColor;
-
-		this.logElm = document.querySelector('.log');
-
-		let timer = null;
-		const startBtn = document.querySelector('.startBtn');
-		startBtn.addEventListener('click', function () {
-			if (timer === null) {
-				let seconds = 0;
-				timer = setInterval(function () {
-					seconds++;
-					display.innerHTML = seconds;
-					console.log(seconds);
-				}, 1000);
-				this.addMsg('開始');
-			}
+		const nextButtonElm = this.rootElm.querySelector('.nextButton');
+		nextButtonElm.addEventListener('click', () => {
+			this.next();
 		});
+
+		const prevButtonElm = this.rootElm.querySelector('.prevButton');
+		prevButtonElm.addEventListener('click', () => {
+			this.prev();
+		});
+
+		this.updatePhoto();
 	}
 
-	addMsg(message) {
-		const msgElm = document.createElement('div');
-		const now = new Date();
-		msgElm.innerHTML = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒 ${message}`;
-		const logElm = document.querySelector('.log');
-		logElm.appendChild(msgElm);
+	updatePhoto() {
+		const frameElm = this.rootElm.querySelector('.frame');
+		const image = this.images[this.currentIndex];
+		frameElm.innerHTML = `
+		<div class="currentImage">
+			<img src="${image}" />
+		</div>
+		`;
+	}
+
+	next() {
+		this.currentIndex++;
+		this.updatePhoto();
+	}
+
+	prev() {
+		this.currentIndex--;
+		this.updatePhoto();
 	}
 }
 
-const options = {
-	color: 'lightgreen',
-	backgroundColor: '#333'
-}
-
-const stopWatch = new StopWatch(options);
-stopWatch.init();
+const images = [
+	'https://fakeimg.pl/250x150/81DAF5',
+	'https://fakeimg.pl/250x150/F781F3',
+	'https://fakeimg.pl/250x150/81F7D8'
+];
+new PhotoViewer(document.getElementById('photoViewer'), images).init();
