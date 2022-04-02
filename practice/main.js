88e0036a10621bdd9aa6b28f6d8fcaa7027f06bd
furukawa <tennis856@gmail.com>
2022-04-03 03:00:19 +0900

    class

--- practice/main.js
+++ practice/main.js
@@ -1,62 +1,71 @@
-/* eslint no-unused-vars: 0 */
-const rootElm = document.getElementById('areaSelector');
-
-async function initAreaSelector() {
-	await updatePref();
-	await updateCity();
-}
-
-async function getPrefs() {
-	const prefResponse = await fetch('./prefectures.json');
-	return await prefResponse.json();
-}
-
-async function getCities(prefCode) {
-	const cityResponse = await fetch(`./cities/${prefCode}.json`);
-	return await cityResponse.json();
-}
-
-async function updatePref() {
-	const prefs = await getPrefs();
-	createPrefOptionsHtml(prefs);
-}
-
-async function updateCity() {
-	const prefSelectorElm = rootElm.querySelector('.prefectures');
-	const cities = await getCities(prefSelectorElm.value);
-	createCityOptionsHtml(cities);
-}
-
-function createPrefOptionsHtml(prefs) {
-	const optionStrs = [];
-	for (const pref of prefs) {
-		optionStrs.push(`
-      <option name="${pref.name}" value="${pref.code}">
-        ${pref.name}
-      </option>
-    `);
+class AreaSelector {
+	constructor(rootElm) {
+		this.rootElm = rootElm;
+		This.pref = [];
+		this.cities = [];
+		this.prefCode = [];
+	}
+	async init() {
+		await this.updatePref();
+		await this.updateCities();
 	}
 
-	const prefSelectorElm = rootElm.querySelector('.prefectures');
-	prefSelectorElm.innerHTML = optionStrs.join('');
-
-	prefSelectorElm.addEventListener('change', (event) => {
-		updateCity();
-	});
-}
-
-function createCityOptionsHtml(cities) {
-	const optionStrs = [];
-	for (const city of cities) {
-		optionStrs.push(`
-      <option name="${city.name}" value="${city.code}">
-        ${city.name}
-      </option>
-    `);
+	async initAreaSelector() {
+		await updatePref();
+		await updateCity();
 	}
 
-	const citySelectorElm = rootElm.querySelector('.cities');
-	citySelectorElm.innerHTML = optionStrs.join('');
+	async getPrefs() {
+		const prefResponse = await fetch('./prefectures.json');
+		return await prefResponse.json();
+	}
+
+	async getCities(prefCode) {
+		const cityResponse = await fetch(`./cities/${prefCode}.json`);
+		return await cityResponse.json();
+	}
+
+	async updatePref() {
+		const prefs = await getPrefs();
+		createPrefOptionsHtml(prefs);
+	}
+
+	async updateCity() {
+		const prefSelectorElm = this.rootElm.querySelector('.prefectures');
+		const cities = await getCities(prefSelectorElm.value);
+		createCityOptionsHtml(cities);
+	}
+
+	createPrefOptionsHtml(prefs) {
+		const optionStrs = [];
+		for (const pref of prefs) {
+			optionStrs.push(`
+				<option name="${pref.name}" value="${pref.code}">
+					${pref.name}
+				</option>
+			`);
+		}
+
+		const prefSelectorElm = this.rootElm.querySelector('.prefectures');
+		prefSelectorElm.innerHTML = optionStrs.join('');
+
+		prefSelectorElm.addEventListener('change', (event) => {
+			updateCity();
+		});
+	}
+
+	createCityOptionsHtml() {
+		const citySelectorElm = this.rootElm.querySelector('.cities');
+		citySelectorElm.innerHTML = optionStrs.toOptionsHtml(this.cities);
+	}
+
+	toOptionsHtml(records) {
+		return records.map((record) => {
+			return `<option value="${record.name}"> value="${record.value}">${record.name}</option>`
+		}).join('');
+	}
+
 }
 
-initAreaSelector();
+const areaSelector = new AreaSelector(document.getElementById('areaSelector'));
+areaSelector.init();
